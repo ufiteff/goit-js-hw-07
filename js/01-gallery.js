bd432695d914c2e422f52ffa1841a79acc8481fa
Hanna Andriushchenko <ufi.teff@gmail.com>
2022-11-02 09:01:07 +0200

    changes3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,17 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-//const item =
-//` <div class="gallery__item">
-  //<a class="gallery__link" href="large-image.jpg">
-    //<img
-      //class="gallery__image"
-      //src="small-image.jpg"
-      //data-source="large-image.jpg"
-     // alt="Image description"
-    ///>
-  //</a>
-//</div> `
+
 function createItemCards (arrey){
     return arrey.map((el)=>{
         const {description, original, preview} = el
@@ -37,19 +27,20 @@ const list = document.querySelector('.gallery');
 list.insertAdjacentHTML("afterbegin", markup);
 
 
-function clickOnImagePreview(e) {
-    e.preventDefault();
-    if (e.target.nodeName !== "IMG") {
-    return;
-  }
-    
-    const instance = basicLightbox.create(` 
-        <img src=${e.target.dateset.source} width="800" height="600">
-    `);
-    
-}
-    
-     instance.show()
+
+
+
+function showInstance() {
+e.preventDefault();
+ if (e.target.nodeName !== "IMG") {
+ return;
+ }
+ const instance = basicLightbox.create(` 
+ <img src="${e.target.dataset.source}">
+ `);
+
+instance.show() 
+}  
 
       
 function closeModalOnPressEscape(event) {
