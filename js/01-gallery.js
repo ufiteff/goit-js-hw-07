f713ef7c736b1c3b320fd4239cde1e526f751114
Hanna Andriushchenko <ufi.teff@gmail.com>
2022-11-19 09:59:12 +0200

    changes5

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-
+const item =
+` <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> `
 function createItemCards (arrey){
     return arrey.map((el)=>{
         const {description, original, preview} = el
-        
+        //console.log (el)
             return`
             <div class="gallery__item">
-            <a class="gallery__link" href="${original}">
+            <a class="gallery__link" href='${original}'>
               <img
                 class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
+                src='${preview}'
+                data-source='${original}'
+                alt='${description}'
               />
             </a>
           </div>`
@@ -27,21 +36,25 @@ const list = document.querySelector('.gallery');
 list.insertAdjacentHTML("afterbegin", markup);
 
 
-
-
-
-function showInstance() {
-e.preventDefault();
- if (e.target.nodeName !== "IMG") {
- return;
- }
- const instance = basicLightbox.create(` 
- <img src="${e.target.dataset.source}">
+list.addEventListener(`click`, onGalleryItemClick)
+const instance = basicLightbox.create(`
+   <img src="assets/images/image.png" width="800" height="600">
  `);
-
-instance.show() 
-}  
-
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  galleryItems.map((item) => {
+    if (item.original === event.target.dataset.source) {
+      const instance = basicLightbox.create(` 
+        <img src=${item.original} width="800" height="600">
+    `);
+    instance.show();
+    }
+   }
+  )   
+ }
       
 function closeModalOnPressEscape(event) {
     if (event.code !== "Escape") {
